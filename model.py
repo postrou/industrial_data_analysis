9aeebf1f6a026efd9efd4439724cbc0eca0a6fb6
ostroukhov <ostroukhov@phystech.edu>
2018-03-22 21:20:50 +0300

    added model

---
+++ model.py
@@ -0,0 +1,13 @@
+from sklearn.linear_model import LinearRegression
+
+
+class RegressionModel(object):
+
+    def __init__(self):
+        self.model = LinearRegression(n_jobs=4)
+
+    def fit(self, X, y):
+        self.model.fit(X, y)
+
+    def predict(self, X):
+        self.model.predict(X)
