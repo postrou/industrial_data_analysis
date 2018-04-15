4871689d3bcf7e8f95d52b9f48374cb8c67e5c0b
ostroukhov <ostroukhov@phystech.edu>
2018-04-15 22:44:10 +0300

    added some model tests

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import numpy as np
 from sklearn.linear_model import LinearRegression
 
 
@@ -6,13 +7,14 @@ class RegressionModel(object):
     """
     Regression model class.
 
-    As regression model the linear regression from scikit-learn wast taken. Takes nothing as input.
+    As regression model the linear regression from scikit-learn was taken. Takes nothing as input.
     """
 
     def __init__(self):
         """
         Initialize a `RegressionModel` object.
         """
+
         self.model = LinearRegression(n_jobs=4)
 
     def fit(self, X, y):
@@ -24,6 +26,10 @@ class RegressionModel(object):
         - `X`: object-feature learn matrix
         - `y`: `y` values form `f(X) = y`
         """
+
+        for j in range(X.shape[1]):
+            if np.array_equal(X[:, j], y):
+                raise ValueError
         self.model.fit(X, y)
 
     def predict(self, X):
@@ -34,4 +40,5 @@ class RegressionModel(object):
 
         - `X`: object-feature predict matrix
         """
+
         return self.model.predict(X)
